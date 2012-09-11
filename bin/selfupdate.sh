f41fa0a3c77f4a57b8c04da18a7206239033c04a
Tom Howe <turtlebender@gmail.com>
2012-09-11 11:39:50 -0500

    add github configuration

--- bin/selfupdate.sh
+++ bin/selfupdate.sh
@@ -1,7 +1,34 @@
 #!/usr/bin/env bash
 
+echo "Checking for updates"
 [ "xxx${SNAKEGIT_HOME}" == "xxx" ] && SNAKEGIT_HOME=${HOME}/.snakegit
 
 cd $SNAKEGIT_HOME
 
 git pull origin master
+
+git config --get-regexp github.* > /dev/null
+
+function register_github {
+echo "Registering with github"
+}
+
+function unregister_github {
+echo "Unregister with github"
+}
+
+if [ $? -eq 0 ]
+then
+  echo "You already have github configured in your ~/.gitconfig"
+  echo ""
+  echo "Do you want to reregister your account [n]?"
+  read REGISTER
+  if [ "$REGISTER" == "y" ] || [ "$REGISTER" == "Y" ]
+  then
+    unregister_github
+    register_github
+  fi
+else
+  register_github
+fi
+
