4405c93858d93e972e7712e0ba3368f8dd6f6329
Tom Howe <turtlebender@gmail.com>
2012-09-11 10:07:01 -0500

    fix missing ]

--- bin/virtualenv.sh
+++ bin/virtualenv.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-[ "${DEV_TOOLS_HOME}xxx" == "xxx" && DEV_TOOLS_HOME=${HOME}/.snakegit
+[ "${DEV_TOOLS_HOME}xxx" == "xxx" ] && DEV_TOOLS_HOME=${HOME}/.snakegit
 
 VIRTUALENV_SRC=$DEV_TOOLS_HOME/var/submodules/virtualenv
-[ "${VIRTUALENV_DIR}xxx" == "xxx" && VIRTUALENV_DIR=`pwd`/vendor/python
+[ "${VIRTUALENV_DIR}xxx" == "xxx" ] && VIRTUALENV_DIR=`pwd`/vendor/python
 
 if [ ! -f $VIRTUALENV_DIR/bin/python ]
 then
