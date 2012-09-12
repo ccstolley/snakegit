9fbc1010d0880d03d83d8658c26789f0def87d05
Tom Howe <turtlebender@gmail.com>
2012-09-12 07:57:14 -0500

    use the main virtualenv installation
    Instead of using the script, use the installed version of
    virtualenv.

--- bin/virtualenv.sh
+++ bin/virtualenv.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-[ "${DEV_TOOLS_HOME}xxx" == "xxx" ] && DEV_TOOLS_HOME=${HOME}/.snakegit
+[ "${SNAKEGIT_HOME}xxx" == "xxx" ] && SNAKEGIT_HOME=${HOME}/.snakegit
 
-VIRTUALENV_SRC=$DEV_TOOLS_HOME/var/submodules/virtualenv
+VIRTUALENV_SRC=$SNAKEGIT_HOME/var/submodules/virtualenv
 [ "${VIRTUALENV_DIR}xxx" == "xxx" ] && VIRTUALENV_DIR=`pwd`/vendor/python
 
 if [ ! -f $VIRTUALENV_DIR/bin/python ]
 then
-	/usr/bin/env python $VIRTUALENV_SRC/virtualenv.py --no-site-packages --distribute $VIRTUALENV_DIR
+	/usr/bin/env python $SNAKEGIT_HOME/bin/virtualenv --distribute $VIRTUALENV_DIR
 	rm -f distribute*.tar.gz
 fi
