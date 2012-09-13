449df88acf60608229ac41042a8b28353215d0a5
Tom Howe <turtlebender@gmail.com>
2012-09-13 12:55:17 -0500

    moving PYTHONPATH out of loop

--- bin/sphinx.sh
+++ bin/sphinx.sh
@@ -11,9 +11,9 @@ if [ -d "${DOC_SRC_DIR}" ]
 then
 	echo "Running sphinx-apidoc..."
   PACKAGES=`${VIRTUALENV_DIR}/bin/python setup.py --provides`
+	export PYTHONPATH=${PWD}/src:${PWD}:${VIRTUALENV_DIR}/lib/python2.7/site-packages:${VIRTUALENV_DIR}/lib/python2.6/site-packages
 	for p in $PACKAGES
 	do
-		export PYTHONPATH=${PWD}/src:${PWD}:${VIRTUALENV_DIR}/lib/python2.7/site-packages:${VIRTUALENV_DIR}/lib/python2.6/site-packages
 		${SNAKEGIT_HOME}/bin/sphinx-apidoc -f -o ${DOC_SRC_DIR} src/$p
 	done
 	${SNAKEGIT_HOME}/bin/sphinx-build ${DOC_SRC_DIR} ${DOC_BUILD_DIR}
