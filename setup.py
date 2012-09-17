f1be408149080a454c9e9f3995db5d0b7c91f858
Tom Howe <turtlebender@gmail.com>
2012-09-17 12:19:53 -0500

    intiial setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import ConfigParser
+import setuptools
+
+config = ConfigParser.RawConfigParser()
+config.read("snake.cfg")
+
+scripts = ["{0}={1}".format(key, config.get('commands', key)) for key in config.options("commands")]
+
+
+with open("requirements.txt", 'r') as f:
+	requires = [req.strip() for req in f.readlines()]
+setuptools.setup(
+		name=config.get("release", "name"),
+		version=config.get("release", "version"),
+		description=config.get("release", "description"),
+		package_dir={"":"src"},
+		packages=setuptools.find_packages("src"),
+		install_requires=requires,
+		package_data = {
+			"snakes": ["nyan"]
+			},
+		entry_points = {
+			"snake_scripts": scripts,
+			"console_scripts": scripts 
+			}
+		)
+
