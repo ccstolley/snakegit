ac272f7ca57172725b5168b480ee13f40fff0a2d
Tom Howe <turtlebender@gmail.com>
2012-09-17 14:36:05 -0500

    remove old binary files

--- bin/sdist.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-
-[ "${DEV_TOOLS_HOME}xxx" = "xxx" ]  && DEV_TOOLS_HOME=${HOME}/.snakegit
-[ "${VIRTUALENV_DIR}xxx" = "xxx" ]  && VIRTUALENV_DIR=vendor/python
-
-/usr/bin/env bash $DEV_TOOLS_HOME/bin/build.sh
-
-$VIRTUALENV_DIR/bin/python setup.py sdist
--- bin/upload.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-[ "${NS_DEV_UTILS}xxx" = "xxx" ] && NS_DEV_UTILS=~/.snakegit
-
-echo "Uploading file"
-FILE_TO_UPLOAD=`ls -rt dist/|tail -1`
-test "$FILE_TO_UPLOAD" = "" && echo "You must generate an sdist first using git sdist" && exit 1
-echo "Uploading $FILE_TO_UPLOAD"
-curl -k -F upload_file=@dist/$FILE_TO_UPLOAD -u `git config --get pypi.user` `git config --get pypi.scheme`://`git config --get pypi.host`/upload
