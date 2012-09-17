1589514c2f55e07ff469aea09fadf149010cfeea
Tom Howe <turtlebender@gmail.com>
2012-09-17 16:06:25 -0500

    adding features for jenkins

--- src/snakes/build.py
+++ src/snakes/build.py
@@ -2,6 +2,7 @@
 
 import snakes.util
 import os
+import os.path
 
 def main():
 	"""docstring for main"""
@@ -13,6 +14,9 @@ def main():
 		cache = os.environ['VENV_CACHE_HOME']
 	else:
 		cache = 'vendor/cache'
+	home = os.environ.get("SNAKEGIT_HOME", os.path.expanduser('~/.snakegit'))
+	if not os.path.exists(os.path.join(venv, 'bin', 'python')):
+		snakes.util.runcmd('{0} --distribute {1}'.format(os.path.join(home, 'bin', 'virtualenv'), venv))
 	cmd = '{0}/bin/pip install --find-links=file://{1} --no-index --index-url=file:///dev/null --no-deps -r requirements.txt'.format(venv, cache)
 	snakes.util.run_cmd(cmd)
 	cmd = '{0}/bin/python setup.py develop'.format(venv)
--- src/snakes/config.py
+++ src/snakes/config.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+import getpass
+import json
 import os
 import os.path
 import urllib2
@@ -8,36 +10,65 @@ import shutil
 import subprocess
 import sys
 
+import clint.textui.colored
+import git
+import requests
+import snakes.util
+
+
+def register_github(writer, user=''):
+	print "Register github"
+	request_data = {
+				"scopes": ["repo", "gist"],
+				"note": "SnakeGit tools"
+				}
+	url = 'https://api.github.com/authorizations'
+	user = raw_input("What is your github username [{0}]? ".format(user))
+	password = getpass.getpass("What is your github password? ")
+	result = requests.post(url, data=json.dumps(request_data), auth=(user, password))
+
+	data = result.json
+
+	writer.set('github', 'user', user)
+	writer.set('github', 'token', data['token'])
+	writer.set('github', 'url', data['url'])
+
+def register_pypi(writer):
+	print "Register with Pypi"
+	url = 'https://repo.n-s.us/token'
+	user = raw_input('What is your PyPi username? ')
+	password = getpass.getpass("What is your PyPi password?")
+	result = requests.post(url, auth=(user, password), verify=False)
+	print result
+
 
 def main():
 	"""docstring for main"""
-	home = os.environ.get('SNAKEGIT_HOME', os.path.expanduser('~/.snakegit'))
-	if not os.path.exists(os.path.join(home, 'bin', 'python')):
-		virtualenv_src = urllib2.urlopen('https://raw.github.com/pypa/virtualenv/master/virtualenv.py').read()
-		fd, path = tempfile.mkstemp()
-		os.write(fd, virtualenv_src)
-		os.close(fd)
-		if os.path.exists('/tmp/virtualenv.py'):
-			os.remove('/tmp/virtualenv.py')
-
-		subprocess.call("python {0} --distribute {1}".format(path, home), shell=True)
-		os.remove(path)
-	current_dir = os.getcwd()
-	os.chdir(home)
-	cmd = '{0}/bin/pip install --find-links=file://{1} --no-index --index-url=file:///dev/null --no-deps -r requirements.txt'.format(home, os.path.join(home, 'vendor', 'cache'))
-	subprocess.call(cmd, shell=True)
-	subprocess.call('{0}/bin/python setup.py install'.format(home), shell=True)
-	os.chdir(current_dir)
-	try:
-		import git
-	except ImportError:
-		sys.path.append(os.path.join(home, 'lib', 'python2.6', 'site-packages'))
-		sys.path.append(os.path.join(home, 'lib', 'python2.7', 'site-packages'))
-		import git
+	home = os.environ.get("SNAKEGIT_HOME", os.path.expanduser('~/.snakegit'))
 	repo = git.Repo(home)
 	writer = repo.config_writer(config_level='global')
 	writer.set('alias', 'snake', '! {0}/bin/snake'.format(home))
 	writer.write()
+
+	reader = repo.config_reader(config_level='global')
+	if reader.has_option('github', 'url'):
+		reset_github = clint.textui.colored.yellow('''
+You already seem to have github configured.
+Do you want to reset it [n]? ''')
+		response = raw_input(reset_github)
+		if response.strip().lower() in  ['y', 'yes', 'yea', 'yeah']:
+			register_github(writer, reader.get('github', 'user'))
+	else:
+		register_github(writer)
+	if reader.has_option('pypi', 'user'):
+		reset_pypi = clint.textui.colored.yellow('''
+You already seem to have pypi configured.
+Do you want to reset it [n]? ''')
+		response = raw_input(reset_pypi)
+		if response.strip().lower() in  ['y', 'yes', 'yea', 'yeah']:
+			register_pypi(writer)
+	else:
+		register_pypi(writer)
 
 if __name__ == '__main__':
 	main()
