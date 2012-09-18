be9248d4e636b8f9c6b04fd8b2799814dcdccf0f
Tom Howe <turtlebender@gmail.com>
2012-09-18 11:25:52 -0500

    replacing snake_util with subcommand

--- src/snakes/build.py
+++ src/snakes/build.py
@@ -16,7 +16,7 @@ def main():
 		cache = 'vendor/cache'
 	home = os.environ.get("SNAKEGIT_HOME", os.path.expanduser('~/.snakegit'))
 	if not os.path.exists(os.path.join(venv, 'bin', 'python')):
-		snakes.util.runcmd('{0} --distribute {1}'.format(os.path.join(home, 'bin', 'virtualenv'), venv))
+		subprocess.call('{0} --distribute {1}'.format(os.path.join(home, 'bin', 'virtualenv'), venv))
 	cmd = '{0}/bin/pip install --find-links=file://{1} --no-index --index-url=file:///dev/null --no-deps -r requirements.txt'.format(venv, cache)
 	subprocess.call(cmd, shell=True)
 	cmd = '{0}/bin/python setup.py develop'.format(venv)
