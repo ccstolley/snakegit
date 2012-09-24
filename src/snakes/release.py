f4460219f661a422a0fb9b9052fd1cd00051daf4
Tom Howe <turtlebender@gmail.com>
2012-09-24 12:12:55 -0500

    adding support for gearbox releases

--- src/snakes/release.py
+++ src/snakes/release.py
@@ -1,17 +1,22 @@
 #!/usr/bin/env python
 
 import argparse
+import ConfigParser
 import os
 import os.path
+import shutil
 import subprocess
 import sys
 
+import boto
+import boto.s3.key
 import git
 import requests
 
 venv = os.environ.get('VIRTUALENV_HOME', os.path.abspath('vendor/python'))
 cache = os.environ.get('VENV_CACHE_HOME', os.path.abspath('vendor/cache'))
 home = os.environ.get("SNAKEGIT_HOME", os.path.expanduser('~/.snakegit'))
+bucket_name = os.environ.get('GEARBOX_BUCKET', 's3_ops')
 
 def upload():
     parser = argparse.ArgumentParser()
@@ -26,6 +31,18 @@ def upload():
             print "Uploaded successfully"
         else:
             print "Error uploading package"
+
+def upload_gearbox_app():
+    parser = ConfigParser.RawConfigParser()
+    parser.read(os.path.abspath('snake.cfg'))
+    name = parser.get('release', 'name')
+    version = parser.get('release', 'version')
+    s3_conn = boto.connect_s3()
+    bucket = s3_conn.get_bucket(bucket_name)
+    key = boto.s3.key.Key(bucket)
+    key.key = '{0}.tar.gz'.format(version)
+    key.get_contest_from_filename('gearbox_dist/{0}.tar.gz'.format(version))
+    print "Uploaded gearbox update"
     
 def python_sdist():
     print "Building python source distribution" 
@@ -34,10 +51,44 @@ def python_sdist():
             "sdist"]
     subprocess.call(cmd)
 
+def gearbox_dist():
+    print "Build gearbox distribution"
+    cmd = ["{0}/bin/python".format(venv),
+            "setup.py",
+            "install"]
+    subprocess.call(cmd)
+    cmd = ["{0}/bin/virtualenv".format(home),
+            "--relocatable",
+            venv]
+    subprocess.call(cmd)
+    if os.path.exists('./gearbox'):
+        shutil.rmtree('./gearbox')
+    os.mkdir('./gearbox')
+    cmd = ["rsync",
+        "-arv",
+        "{0}/".format(venv),
+        "gearbox/"]
+    subprocess.call(cmd)
+    cwd = os.getcwd()
+    if not os.path.exists('./gearbox_dist'):
+        os.mkdir('./gearbox_dist')
+    parser = ConfigParser.RawConfigParser()
+    parser.read(os.path.abspath('./snake.cfg'))
+    os.chdir('gearbox')
+    cmd = ["tar",
+            "-czvf",
+            "../gearbox_dist/{0}.tar.gz".format(parser.get('release', 'version')),
+            "."]
+    subprocess.call(cmd)
+    os.chdir(cwd)
+
 def create():
     if os.path.exists(os.path.abspath('./setup.py')) \
             and not os.path.exists(os.path.abspath('./_gb')):
         python_sdist()
+    if os.path.exists(os.path.abspath('./_gb')):
+        gearbox_dist()
+
 
 def main():
     """docstring for main"""
