6543b6c07b9549e713d5927b6730a93795d6702f
Tom Howe <turtlebender@gmail.com>
2012-09-26 05:33:12 -0500

    replacing shell with array in subprocess call.
    some additional formatting

--- src/snakes/deps.py
+++ src/snakes/deps.py
@@ -5,16 +5,22 @@ import os
 import os.path
 import shutil
 import subprocess
-import git
+import re
 import sys
+import urlparse
+
+import git
+import yolk.pypi
 
 home = os.environ.get('SNAKEGIT_HOME', os.path.expanduser('~/.snakegit'))
 venv = os.path.abspath(os.environ.get('VIRTUALENV_HOME', 'vendor/python'))
 cache = os.path.abspath(os.environ.get('VENV_CACHE_HOME', 'vendor/cache'))
 
+cheese_shop = yolk.pypi.CheeseShop()
+
 def sync():
-    shutil.rmtree(cache)
-    os.makedirs(cache)
+    if not os.path.exists(cache):
+        os.makedirs(cache)
     repo = git.Repo(home)
     reader = repo.config_reader()
     if not reader.has_section('pypi'):
@@ -22,11 +28,33 @@ def sync():
         sys.exit(1)
     uid = reader.get('pypi', 'user')
     password = reader.get('pypi', 'key')
-    cmd = "pip install --no-install -d vendor/cache/ --no-deps -i https://{0}:{1}@repo.n-s.us/simple -r requirements.txt".format(uid, password)
-    subprocess.call(cmd, shell=True)
+
+    os.path.exists(os.path.join('vendor','cache', urlparse.urlparse(dl).path.split('/')[-1]))
+    cmd = [
+            "pip",
+            'install',
+            '--no-install',
+            '-d',
+            'vendor/cache/',
+            '--no-deps',
+            '--use-mirrors',
+            '--extra-index-url https://{0}:{1}@repo.n-s.us/simple'.format(uid,password),
+            '-r',
+            'requirements.txt'
+            ]
+    subprocess.call(cmd)
     if os.path.exists(os.path.abspath('./test-requirements')):
-        cmd = "yes w | pip install --no-install -d vendor/cache/ --no-deps -r test-requirements.txt"
-        subprocess.call(cmd, shell=True)
+        cmd = [
+                'pip',
+                'install',
+                '--no-install',
+                '-d',
+                'vendor/cache/',
+                '--no-deps',
+                '-r',
+                'test-requirements.txt'
+                ]
+        subprocess.call(cmd)
 
 
 def main():
--- src/snakes/test.py
+++ src/snakes/test.py
@@ -15,30 +15,38 @@ def unit(args):
     home = os.environ.get('SNAKEGIT_HOME', os.path.expanduser('~/.snakegit'))
     parser = argparse.ArgumentParser()
     parser.add_argument('-x', '--xunit',
-          action='store_const',
-          const='--with-xunit --xunit-file={0}/nosetests.xml',
-          default='',
-          help="Generate XUnit formatted report")
+            action='store_const',
+            const='--with-xunit --xunit-file={0}/nosetests.xml',
+            default='',
+            help="Generate XUnit formatted report")
     parser.add_argument("-c", '--coverage',
-          action='store_const',
-          const='--with-coverage --cover-xml --cover-xml-file={0}/coverage.xml',
-          default='',
-          help="Generate a test coverage report")
+            action='store_const',
+            const='--with-coverage --cover-xml --cover-xml-file={0}/coverage.xml',
+            default='',
+            help="Generate a test coverage report")
     parser.add_argument('-p', '--package',
-          help="Which package should have coverage measured",
-          default='')
+            help="Which package should have coverage measured",
+            default='')
     parser.add_argument('-d', '--directory',
-          help="Which directory holds the tests",
-          default="tests/unit")
+            help="Which directory holds the tests",
+            default="tests/unit")
     parser.add_argument('-o', '--output',
-          help="Which directory should hold the test reports",
-          default=os.path.join(os.getcwd(), 'test_reports'))
+            help="Which directory should hold the test reports",
+            default=os.path.join(os.getcwd(), 'test_reports'))
 
     args = parser.parse_args(args)
 
-    if os.path.exists(os.path.join(os.getcwd(), 'test-requirements.txt')):
-        cmd = '{0}/bin/pip install --find-links=file://{1} --no-index --index-url=file:///dev/null --no-deps -r test-requirements.txt'.format(venv, os.path.abspath(cache))
-        cmd = cmd.split(' ')
+    if os.path.exists('test-requirements.txt'):
+        cmd = [
+                '{0}/bin/pip'.format(venv),
+                'install',
+                '--find-links=file://{0}'.format(os.path.abspath(cache)),
+                '--no-index',
+                '--index-url=file:///dev/null',
+                '--no-deps',
+                '-r',
+                'test-requirements.txt'
+                ]
         subprocess.call(cmd)
     if args.package is not None and args.package != '':
         args.package = '--cover-package={0}'.format(args.package)
@@ -46,13 +54,14 @@ def unit(args):
     if not os.path.exists(args.output):
         os.makedirs(args.output)
     python_path = 'test_configs:src:.:{0}/lib/python2.6/site-packages:{0}/lib/python2.7/site-packages'.format(venv)
-    cmd = "{0}/bin/nosetests {1} {2} {3} {4}".format(
-          home,
-          args.xunit.format(args.output),
-          args.coverage.format(args.output),
-          args.package,
-          args.directory)
-    return subprocess.call(cmd.split(), env={"PYTHONPATH": python_path})
+    cmd = [
+            "{0}/bin/nosetests".format(home),
+            args.xunit.format(args.output),
+            args.coverage.format(args.output),
+            args.package,
+            args.directory
+            ]
+    return subprocess.call(cmd, env={"PYTHONPATH": python_path})
 
 
 def functional():
