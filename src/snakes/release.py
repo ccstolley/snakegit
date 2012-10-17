cb92a531f82b654bf9a66d088c49b8eaa852da4e
Tom Howe <turtlebender@gmail.com>
2012-10-17 11:58:01 -0500

    removing environment from gearbox upload

--- src/snakes/release.py
+++ src/snakes/release.py
@@ -39,10 +39,6 @@ def upload_pypi():
             print "Error uploading package"
 
 def upload_gearbox_app():
-    args = argparse.ArgumentParser()
-    args.add_argument("-e", "--environment",
-            required=True, help="Which environment should this upload to?")
-    args = args.parse_args(sys.argv[2:])
     parser = ConfigParser.RawConfigParser()
     parser.read(os.path.abspath('snake.cfg'))
     name = parser.get('release', 'name')
@@ -50,7 +46,7 @@ def upload_gearbox_app():
     s3_conn = boto.connect_s3()
     bucket = s3_conn.get_bucket(bucket_name)
     key = boto.s3.key.Key(bucket)
-    key.key = '{0}/{1}/{2}.tar.gz'.format(name, args.environment, version)
+    key.key = '{0}/{2}.tar.gz'.format(name, version)
     key.set_contents_from_filename('gearbox_dist/{0}.tar.gz'.format(version))
     print "Uploaded gearbox update"
     
