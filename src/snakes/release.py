62ae8785b4fef7c66a2e9c0ce3b28888636b2a16
jbouwman <jbouwman@narrativescience.com>
2012-10-25 10:41:21 -0500

    check for presence of virtualenv when releasing

--- src/snakes/release.py
+++ src/snakes/release.py
@@ -3,7 +3,7 @@
 import argparse
 import ConfigParser
 import os
-import os.path
+from os.path import abspath, exists, expanduser
 import shutil
 import subprocess
 import sys
@@ -13,16 +13,23 @@ import boto.s3.key
 import git
 import requests
 
-venv = os.environ.get('VIRTUALENV_HOME', os.path.abspath('vendor/python'))
-cache = os.environ.get('VENV_CACHE_HOME', os.path.abspath('vendor/cache'))
-home = os.environ.get("SNAKEGIT_HOME", os.path.expanduser('~/.snakegit'))
+venv = os.environ.get('VIRTUALENV_HOME', abspath('vendor/python'))
+cache = os.environ.get('VENV_CACHE_HOME', abspath('vendor/cache'))
+home = os.environ.get("SNAKEGIT_HOME", expanduser('~/.snakegit'))
 bucket_name = os.environ.get('GEARBOX_BUCKET', 's3_ops')
 
+def _python_bin():
+    '''Return path to virtualenv python'''
+    if not exists(venv):
+        msg = "Missing virtualenv: have you run 'build'? (looked in: {0})"
+        raise RuntimeError(msg.format(venv))
+    return abspath("{0}/bin/python".format(venv))
+
 def upload():
-    if os.path.exists(os.path.abspath('./setup.py')) \
-            and not os.path.exists(os.path.abspath('./_gb')):
+    if exists(abspath('./setup.py')) \
+            and not exists(abspath('./_gb')):
         upload_pypi()
-    if os.path.exists(os.path.abspath('./_gb')):
+    if exists(abspath('./_gb')):
         upload_gearbox_app()
 
 def upload_pypi():
@@ -30,9 +37,12 @@ def upload_pypi():
     reader = repo.config_reader()
     uid = reader.get('pypi', 'user')
     key = reader.get('pypi', 'key')
-    upload_file = subprocess.check_output("ls -rt dist/|tail -1", shell=True).strip()
-    with open(os.path.abspath(os.path.join('dist',upload_file)), 'r') as f:
-        r = requests.post("https://repo.n-s.us/upload", auth=(uid, key), files={'upload_file': (upload_file, f) }, verify=False)
+    filename_cmd = "ls -rt dist/|tail -1"
+    upload_file = subprocess.check_output(filename_cmd, shell=True).strip()
+    with open(abspath(join('dist',upload_file)), 'r') as f:
+        r = requests.post("https://repo.n-s.us/upload", auth=(uid, key),
+                          files={'upload_file': (upload_file, f) },
+                          verify=False)
         if r.status_code == 201:
             print "Uploaded successfully"
         else:
@@ -40,7 +50,7 @@ def upload_pypi():
 
 def upload_gearbox_app():
     parser = ConfigParser.RawConfigParser()
-    parser.read(os.path.abspath('snake.cfg'))
+    parser.read(abspath('snake.cfg'))
     name = parser.get('release', 'name')
     version = parser.get('release', 'version')
     s3_conn = boto.connect_s3()
@@ -49,25 +59,21 @@ def upload_gearbox_app():
     key.key = '{0}/{1}.tar.gz'.format(name, version)
     key.set_contents_from_filename('gearbox_dist/{0}.tar.gz'.format(version))
     print "Uploaded gearbox update"
-    
+
 def python_sdist():
-    print "Building python source distribution" 
-    cmd = ["{0}/bin/python".format(venv),
-            "setup.py",
-            "sdist"]
+    print "Building python source distribution"
+    cmd = [_python_bin(), "setup.py", "sdist"]
     subprocess.call(cmd)
 
 def gearbox_dist():
     print "Build gearbox distribution"
-    cmd = ["{0}/bin/python".format(venv),
-            "setup.py",
-            "install"]
+    cmd = [_python_bin(), "setup.py", "install"]
     subprocess.call(cmd)
     cmd = ["{0}/bin/virtualenv".format(home),
             "--relocatable",
             venv]
     subprocess.call(cmd)
-    if os.path.exists('./gearbox'):
+    if exists('./gearbox'):
         shutil.rmtree('./gearbox')
     os.mkdir('./gearbox')
     cmd = ["rsync",
@@ -81,23 +87,24 @@ def gearbox_dist():
         "gearbox/gbtemplate/"]
     subprocess.call(cmd)
     cwd = os.getcwd()
-    if not os.path.exists('./gearbox_dist'):
+    if not exists('./gearbox_dist'):
         os.mkdir('./gearbox_dist')
     parser = ConfigParser.RawConfigParser()
-    parser.read(os.path.abspath('./snake.cfg'))
+    parser.read(abspath('./snake.cfg'))
     os.chdir('gearbox')
+    version = parser.get('release', 'version')
     cmd = ["tar",
             "-czvf",
-            "../gearbox_dist/{0}.tar.gz".format(parser.get('release', 'version')),
+            "../gearbox_dist/{0}.tar.gz".format(version),
             "."]
     subprocess.call(cmd)
     os.chdir(cwd)
 
 def create():
-    if os.path.exists(os.path.abspath('./setup.py')) \
-            and not os.path.exists(os.path.abspath('./_gb')):
+    if exists(abspath('./setup.py')) \
+            and not exists(abspath('./_gb')):
         python_sdist()
-    if os.path.exists(os.path.abspath('./_gb')):
+    if exists(abspath('./_gb')):
         gearbox_dist()
 
 
