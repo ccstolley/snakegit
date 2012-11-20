5505688d5867f83d2412b97f7ec8e2a6a48512d4
Tom Howe <turtlebender@gmail.com>
2012-11-20 09:04:33 -0600

    make build simpler and cache the dependencies
    One of the biggest issues people have had with snakegit has been
    the two stage build (deps then build).  These changes combine all
    of the work into the build step.  It also allows for downloading
    dependencies.  It still caches the dependencies, but it uses the
    cache format.  Also, as part of this, the pypi server has been
    configured correctly to proxy to the central pypi server for packages
    that are not currently available on the ns server

--- src/snakes/build.py
+++ src/snakes/build.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python
 
-import subprocess
 import os
 import os.path
 import sys
 
+import git
+import sh
+
 def main():
     """docstring for main"""
+    home = os.environ.get("SNAKEGIT_HOME", os.path.expanduser('~/.snakegit'))
+    virtualenv = sh.Command("{0}/bin/virtualenv".format(home))
     if 'VIRTUALENV_HOME' in os.environ:
         venv = os.environ['VIRTUALENV_HOME']
     else:
@@ -15,25 +19,28 @@ def main():
         cache = os.environ['VENV_CACHE_HOME']
     else:
         cache = os.path.abspath('vendor/cache')
-    home = os.environ.get("SNAKEGIT_HOME", os.path.expanduser('~/.snakegit'))
     if not os.path.exists(os.path.join(venv, 'bin', 'python')):
-        cmd = [
-                os.path.join(home, 'bin', 'virtualenv'),
-                '--distribute',
-                venv
-                ]
-        subprocess.call(cmd)
-    cmd = [
-            '{0}/bin/pip'.format(venv),
-            'install',
-            '--find-links=file://{0}'.format(cache),
-            '--no-index',
-            '--index-url=file:///dev/null',
-            '--no-deps',
-            '-r',
-            'requirements.txt'
-            ]
-    subprocess.call(cmd)
+        for line in virtualenv(venv, '--distribute', prompt="({0})".format(os.getcwd().split('/')[-1]),
+                _iter=True):
+            sys.stdout.write(line)
+            
+    repo = git.Repo(home)
+    reader = repo.config_reader()
+    if not reader.has_section('pypi'):
+        print "Pypi is not set up yet."
+        sys.exit(1)
+    uid = reader.get('pypi', 'user')
+    password = reader.get('pypi', 'key')
+
+    pip = sh.Command("{0}/bin/pip".format(venv))
+    with open('requirements.txt', 'r') as handle:
+        for req in handle.readlines():
+            for line in pip.install(req, find_links="file://{0}".format(cache),
+                    index_url="https://{0}:{1}@repo.n-s.us/simple".format(uid, password), 
+                    build="build", download_cache=cache, exists_action="i",
+                    _iter=True):
+                sys.stdout.write(line)
+    sh.rm("-rf", "build")
 
 if __name__ == '__main__':
     main()
