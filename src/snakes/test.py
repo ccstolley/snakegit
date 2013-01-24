c706b8a474b8a47e515e6c9bc404cb94b7578728
Ashton Kemerling <ashtonkemerling@gmail.com>
2013-01-24 11:39:19 -0600

    Clean up.

--- src/snakes/test.py
+++ src/snakes/test.py
@@ -83,12 +83,15 @@ def unit(args):
             python_path.append(candidate_path)
             python_version = version
             break
-    if not os.path.exists("{0}/lib/python{1}/no-global-site-packages.txt".format(venv, python_version)):
-        with open("{0}/lib/python{1}/orig-prefix.txt".format(venv, python_version)) as handle:
+    site_packages = "{0}/lib/python{1}/no-global-site-packages.txt".foramt(
+        venv, python_version)
+    prefix_file = "{0}/lib/python{1}/orig-prefix.txt".foramt(venv,
+                                                             python_version)
+    if not os.path.exists(site_packages):
+        with open(prefix_file) as handle:
             prefix = handle.readline()
             python_path.append("{0}/lib/python{1}/site-packages".format(
-                prefix,
-                python_version))
+                prefix, python_version))
     new_env = os.environ.copy()
     new_env["PYTHONPATH"] = ':'.join(python_path)
     nose = sh.Command("{0}/bin/nosetests".format(home))
