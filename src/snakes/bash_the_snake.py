24aad7482cb57741f6f84aaa29a20fabc1c26e5c
waltaskew <waltaskew@gmail.com>
2013-06-14 12:03:08 -0500

    do not rely on return exiting the function
    - return does not actually escape the function on OS x

--- src/snakes/bash_the_snake.py
+++ src/snakes/bash_the_snake.py
@@ -5,10 +5,14 @@ profile_d = """
 function snakeskin()
 {
     export OLDPATH=$PATH
-    test -f snake.cfg || (echo "Not a snake project" ; return 0)
-    test -f vendor/python/bin/activate || git snake build
-    source vendor/python/bin/activate
-    export PATH=$PATH:~/.snakegit/bin
+    if [ -f snake.cfg ]
+    then
+        test -f vendor/python/bin/activate || git snake build
+        source vendor/python/bin/activate
+        export PATH=$PATH:~/.snakegit/bin
+    else
+        echo "Not a Snakegit Project"
+    fi
 }
 
 # when you are done call shed to deactivate the virtualenv
